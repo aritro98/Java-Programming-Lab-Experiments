89c472e3476a1ee2b832e7811d63af0135c3bfae
aritro98 <aritrodutta123@gmail.com>
2025-04-28 13:57:01 +0530

    Added Files

--- machines/Machines.java
+++
@@ -1,8 +0,0 @@
-public class Machines{
-	public static void main(String[] args){
-		Washing_machine m=new Washing_machine();
-		System.out.println("Capacity:"+m.capacity);
-		m.run();
-		m.consume();
-	}
-}
